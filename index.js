bfd8debb04bc6b6d9dd41a1eda05817d756dd277
Siddharth Garg <garg.sid6665@gmail.com>
2022-05-11 19:20:45 +0530

    add gitignore file and connect to data bas

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const express = require('express')
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+
+dotenv.config()
+connectDB()
+
 const app = express()
-const port = 3000
+app.use(express.json())
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
 })
