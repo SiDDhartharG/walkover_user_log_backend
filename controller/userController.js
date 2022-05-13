6d2d67beebc053f1e6e86f7147d9b85ce8bdaba6
Harsh <soni.hs661@gmail.comm>
2022-05-13 15:43:59 +0530

    debug signup login

--- controller/userController.js
+++ controller/userController.js
@@ -1,44 +1,49 @@
-import User from '../models/userModels.js'
-import generateToken from '../utils/token.js'
+import User from "../models/userModels.js";
+import generateToken from "../utils/token.js";
 
 //@route POST /api/user
 //@desc auth user
 const registerUser = async (req, res) => {
-    const { name, email, password } = req.body
+  const { name, email, password } = req.body;
+  try {
     if (name === "" || email === "" || password === "") {
-        res.status(400)
-        throw new Error('Fill All details')
+      throw new Error("Fill All details");
     }
 
-    const userExist = await User.findOne({ email })
+    const userExist = await User.findOne({ email });
     if (userExist) {
-        res.status(400)
-        throw new Error('User already exist')
+      throw new Error("User already exist");
     }
 
-    const user = await User.create({ name, email, password })
+    const user = await User.create({ name, email, password });
     if (user) {
-        res.status(201).json({ token: generateToken(user._id, user.name, user.email) })
+      res
+        .status(201)
+        .json({ token: generateToken(user._id, user.name, user.email) });
     } else {
-        res.status(400)
-        throw new Error('Invalid User data')
+      throw new Error("Invalid User data");
     }
-}
+  } catch (error) {
+    res.status(400).json({ error: error.toString() });
+  }
+};
 
 //@route POST /api/user/login
 //@desc auth user
 const loginUser = async (req, res) => {
-    const { email, password } = req.body
-    const user = await User.findOne({ email })
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
     if (user && (await user.matchPassword(password))) {
-        res.status(201).json({ token: generateToken(user._id, user.name, user.email) })
+      res
+        .status(201)
+        .json({ token: generateToken(user._id, user.name, user.email) });
     } else {
-        res.status(401)
-        throw new Error('incorrect email password')
+      throw new Error("incorrect email password");
     }
-}
+  } catch (error) {
+    res.status(401).json({ error: error.toString() });
+  }
+};
 
-export {
-    registerUser,
-    loginUser
-}
+export { registerUser, loginUser };
--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import connectDB from './config/db.js'
-import userRoutes from './router/userRouter'
-dotenv.config()
-connectDB()
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import userRoutes from "./router/userRouter.js";
+import cors from "cors";
+dotenv.config();
+connectDB();
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(cors({}));
+app.use(express.json());
 
+app.get("/", (_, res) => {
+  res.send("Hello World!");
+});
 
+app.use("/api/user", userRoutes);
 
-app.get('/', (_, res) => {
-    res.send('Hello World!')
-})
-
-
-app.use('/api/user', userRoutes)
-
-
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`)
-})
+  console.log(`Example app listening on port ${PORT}`);
+});
